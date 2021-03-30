b0e24071f477363f03cf2235cd8e3052ff6b41ea
James <liujian01@renmaitech.com>
2021-03-30 16:04:25 +0800

    优化

--- JSONConverter/Classes/Manager/JSONParseManager.swift
+++ JSONConverter/Classes/Manager/JSONParseManager.swift
@@ -27,7 +27,7 @@ class JSONParseManager {
         case let dic as [String: Any]:
             content = handleDictionary(propertyKey: propertyKey, dic: dic)
         case let arr as [Any]:
-            _ = handleArrary(itemKey: propertyKey, arr: arr)
+            _ = handleArray(itemKey: propertyKey, arr: arr)
         default:
             assertionFailure("parse object type error")
         }
@@ -57,8 +57,10 @@ class JSONParseManager {
                 let content = handleDictionary(propertyKey: itemKey, dic: dic)
                 file.contents.insert(content, at: 0)
             case let arr as [Any]:
-                propertyModel = handleArrary(itemKey: itemKey, arr: arr)
-            case  _ as NSNull:
+                propertyModel = handleArray(itemKey: itemKey, arr: arr)
+            case _ as NSNull:
+                fallthrough
+            case nil:
                 propertyModel = file.property(withPropertykey: itemKey, type: .nil)
             default:
                 assertionFailure("parse object type error")
@@ -72,7 +74,7 @@ class JSONParseManager {
         return content
     }
     
-    private func handleArrary(itemKey: String, arr: [Any]) -> Property? {
+    private func handleArray(itemKey: String, arr: [Any]) -> Property? {
         if let first = arr.first {
             var propertyModel: Property?
             switch first {
@@ -81,9 +83,10 @@ class JSONParseManager {
             case let num as NSNumber:
                 let type = PropertyType(rawValue: num.valueType().rawValue + 6)!
                 propertyModel = file.property(withPropertykey: itemKey, type: type)
-            case let dic as [String: Any]:
+            case _ as [String: Any]:
+                let unionDic = unionDictionaryFromArrayElements(arr)
                 propertyModel = file.property(withPropertykey: itemKey, type: .ArrayDictionary)
-                let content = handleDictionary(propertyKey: itemKey, dic: dic)
+                let content = handleDictionary(propertyKey: itemKey, dic: unionDic)
                 file.contents.insert(content, at: 0)
             default:
                 assertionFailure("parse object type error")
@@ -94,6 +97,47 @@ class JSONParseManager {
         }
         
         return nil
+    }
+    
+    /**
+    Creates and returns a dictionary who is built up by combining all the dictionary elements in the passed array.
+
+    - parameter array: array of dictionaries.
+    - returns: dictionary that combines all the dictionary elements in the array.
+    */
+    private func unionDictionaryFromArrayElements(_ array: [Any]) -> [String: Any]
+    {
+//        let dictionary = NSMutableDictionary()
+        var dictionary = [String: Any]()
+        for item in array {
+            if let dic = item as? NSDictionary {
+                //loop all over its keys
+                for key in dic.allKeys as! [String] {
+                    guard let value = dic[key] else { continue }
+                    if value is NSArray {
+                        let t = value as! NSArray
+                        if t.count > 0 {
+                            dictionary[key] = value
+                        }
+                    }
+                    else if value is NSDictionary {
+                        let t = value as! NSDictionary
+                        if t.count > 0 {
+                            dictionary[key] = value
+                        }
+                    }
+                    else {
+                        dictionary[key] = value
+                    }
+                }
+    //            dictionary = dictionary.merging(dic) { (first, second) -> AnyObject in
+    //                if first != nil {
+    //                    return first
+    //                }
+    //            }
+            }
+        }
+        return dictionary
     }
 }
     
